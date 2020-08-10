246b3ac9c759242c41a91f53f53471b7773f0309
ymauri <ymauri@outlook.com>
2020-08-10 21:12:57 +0200

    Set active off on products without stock

---
+++ controllers/front/job.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Cron job for disable out of stock products.
+ * 
+ */
+
+class MallHabanaJobModuleFrontController extends ModuleFrontController {
+    
+    public function postProcess() {
+        Db::getInstance()->execute('UPDATE prstshp_product_shop SET active=0 WHERE id_product IN (SELECT id_product FROM prstshp_stock_available WHERE quantity=0)');
+        // Db::getInstance()->execute('UPDATE prstshp_product_shop SET active=0 WHERE id_product IN (SELECT id_product FROM prstshp_product WHERE quantity=0);');
+        die('done');
+    }
+ }
