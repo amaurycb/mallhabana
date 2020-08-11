0bb84c1bc5bf1fb2fcc1842151b94b8916342b3b
ymauri <ymauri@outlook.com>
2020-08-11 21:42:07 +0200

    Job for update product without stock visibility

--- controllers/front/job.php
+++ controllers/front/job.php
@@ -7,7 +7,8 @@
 class MallHabanaJobModuleFrontController extends ModuleFrontController {
     
     public function postProcess() {
-        Db::getInstance()->execute('UPDATE prstshp_product_shop SET active=0 WHERE id_product IN (SELECT id_product FROM prstshp_stock_available WHERE quantity=0)');
+        Db::getInstance()->execute('UPDATE prstshp_product_shop SET active=0 WHERE id_product IN (SELECT id_product FROM prstshp_stock_available WHERE quantity< 1)');
+        // Db::getInstance()->execute('UPDATE prstshp_product_shop SET active=0 WHERE id_product IN (SELECT id_product FROM prstshp_stock_available WHERE quantity<0)');
         // Db::getInstance()->execute('UPDATE prstshp_product_shop SET active=0 WHERE id_product IN (SELECT id_product FROM prstshp_product WHERE quantity=0);');
         die('done');
     }
